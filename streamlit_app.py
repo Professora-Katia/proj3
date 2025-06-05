5179bab684bc9abf18474fe8846da12dba69ff0d
Professora-Katia <katiambossi@gmail.com>
2025-06-05 19:45:53 -0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="Simulador Integrado", layout="wide")
+st.title("🛫 Simulador de Tráfego Aéreo - Projeto Integrado")
+
+st.markdown("### 👇 Use o menu lateral à esquerda para acessar os módulos:")
+st.markdown("""
+- 🛫 Simulador Principal  
+- 📡 Análise Física: Radar e Vetores  
+- 📐 Análise de Cálculo: Interseção e Newton-Raphson
+""")
+
+
