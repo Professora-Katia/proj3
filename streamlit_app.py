04725a950d1150fe817890a673cc6e67ed035258
Professora-Katia <katiambossi@gmail.com>
2025-06-06 13:15:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,16 @@
 import streamlit as st
 
 st.set_page_config(page_title="Simulador Integrado", layout="wide")
-st.title("🛫 Simulador de Tráfego Aéreo - Projeto Integrado")
+st.title("🛫 Simulador de Tráfego Aéreo")
 
 st.markdown("### 👇 Use o menu lateral à esquerda para acessar os módulos:")
 st.markdown("""
 - 🛫 Simulador Principal  
-- 📡 Análise Física: Radar e Vetores  
-- 📐 Análise de Cálculo: Interseção e Newton-Raphson
+- 📡 Análise 1 Radar e Vetores  
+- 📐 Análise 2: Interseção
 """)
+
+
+
 
 
